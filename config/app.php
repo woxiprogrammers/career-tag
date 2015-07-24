b155178d80e83ffd971b396ad2e5e5eca53f6374
Sagar Acharya <sagar.woxi@gmail.com>
2015-07-24 16:36:36 +0530

    Debugger setup

--- config/app.php
+++ config/app.php
@@ -144,6 +144,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* https://github.com/barryvdh/laravel-debugbar */
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -191,6 +194,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        /* https://github.com/barryvdh/laravel-debugbar */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
